aac02a2f97fa2651f0981d741ca4ffadb4afd5cc
kgalioto <kallia.galiotou@intrasoft-intl.com>
2021-12-08 11:37:45 +0200

    Add HomeScreen Add css for HomePage

--- App.js
+++ App.js
@@ -3,20 +3,28 @@ import { StyleSheet } from 'react-native'
 import { toastConfig } from './src/lib/Toaster'
 import Toast from 'react-native-toast-message'
 import { Navigation } from './src/screens/Navigation'
+ import HomeScreen from './src/screens/HomeScreen';
+import { StatusBar } from "react-native";
 
 export default function App() {
 	const [mainScreenVisible, setMainScreenVisible] = useState(false)
 
 	return (
-		<>
-			<Navigation />
-			<Toast
-				position='top'
-				bottomOffset={70}
-				config={toastConfig}
-				visibilityTime={1500}
-			/>
-		</>
+
+	    <>
+          <StatusBar />
+          <HomeScreen />
+        </>
+
+//		<>
+//			<Navigation />
+//			<Toast
+//				position='top'
+//				bottomOffset={70}
+//				config={toastConfig}
+//				visibilityTime={1500}
+//			/>
+//		</>
 	)
 }
 
